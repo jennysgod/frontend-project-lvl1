f9aa94b3069db48fd0e37d11b99a18c2cbc54713
Misha <jennysgod@gmail.com>
2022-02-16 16:00:17 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 export default function gameEngine(rule, game) {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName} !`);
+  console.log(`Hello, ${userName}!`);
   const maxRounds = 3;
   console.log(`${rule}`);
   let res = 0;
