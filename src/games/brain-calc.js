dc6d0a7d665d6af9bcdca47ca6161dcfb002888f
Misha <jennysgod@gmail.com>
2022-02-17 17:21:34 +0300

    fix brain-calc

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -3,22 +3,21 @@ import randomNum from '../math.js';
 const rule = 'What is the result of the expression?';
 const game = () => {
   const calc = (num1, num2, randomSym) => {
-    let calcRes;
-    if (randomSym === '+') {
-      calcRes = num1 + num2;
+    switch (randomSym) {
+      case '+':
+        return (num1 + num2);
+      case '-':
+        return (num1 - num2);
+      case '*':
+        return (num1 * num2);
+      default:
+        throw new Error('Unknown operator');
     }
-    if (randomSym === '-') {
-      calcRes = (num1 - num2);
-    }
-    if (randomSym === '*') {
-      calcRes = (num1 * num2);
-    }
-    return calcRes;
   };
   const num1 = randomNum();
   const num2 = randomNum();
   const sym = ['+', '-', '*'];
-  const randomSym = sym[Math.floor(Math.random() * sym.length)];
+  const randomSym = sym[randomNum(0, sym.length - 1)];
   const question = `${num1} ${randomSym} ${num2}`;
   const rightAnswer = String(calc(num1, num2, randomSym));
   return [question, rightAnswer];
